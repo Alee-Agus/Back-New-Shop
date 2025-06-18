6921154f1e08b85997675bfd1e04683bb79b2f19
Alee-Agus <aleagusprogramacion1@gmail.com>
2025-06-18 15:48:05 -0300

    Primer commit - proyecto Django backend

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import User
+from .models import Category
+from .models import Product
+from .models import Order
+from .models import OrderItem
+from .models import Payment
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Payment)
+# Register your models here.
